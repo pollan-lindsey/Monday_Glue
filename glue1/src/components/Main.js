b1ffe70407e7191e92bc3010b3377d6ef3dbaf15
LINDSEY POLLAN <LPOLLAN@LIVE.COM>
2022-08-11 16:53:22 -0500

    image added

--- glue1/src/components/Main.js
+++ glue1/src/components/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Image from './prom.jpg' //do this if putting the local image directly in the source doesn't work
 
 
 export default class Main extends Component {
     render(){
         return (
           <>
-          <img src='../prom-solo.JPG' alt='image of me'></img>
+          <img src={Image} width='345' height='420' alt='image of me' />
           <p>I am currently in Code Crew Code School. I have a dog named Belle, a hampster named Rosalie, and 3 red eye tetras. 
           I have too many interests to list</p>
           <a href='https://github.com/pollan-lindsey' target='_blank'>My Github page</a>
